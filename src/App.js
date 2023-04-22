9b409094d75dbf5258f5b099704342f0c7497968
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-04-22 12:14:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+ import {Route} from 'react-router-dom'
+ import './App.css';
+import Welcome from './component/Welcome'
+import Product from './component/Product';
+import Header from './component/Header';
+import ProductDetails from './component/ProductDetails';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header></Header>
+    <main>
+      <Route path='/welcome'>
+        <Welcome></Welcome>
+      </Route>
+      <Route path='/product'>
+        <Product></Product>
+      </Route>
+      <Route path='/product-details'>
+        <ProductDetails></ProductDetails>
+      </Route>
+      </main>
     </div>
   );
 }
